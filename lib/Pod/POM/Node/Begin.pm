1d8ea12ddc2d7aea06da99ac15f758e9f1a5ac05
jonallen <jj@jonallen.info>
2010-09-05 13:08:37 +0100

    Local patch to Pod::POM to fix handling of =begin blocks

--- lib/Pod/POM/Node/Begin.pm
+++ lib/Pod/POM/Node/Begin.pm
@@ -30,7 +30,7 @@ use parent qw( Pod::POM::Node );
 use vars qw( %ATTRIBS @ACCEPT $EXPECT $ERROR );
 
 %ATTRIBS =   ( format => undef );
-@ACCEPT  = qw( text verbatim code );
+@ACCEPT  = qw( text verbatim code over head1 head2 head3 head4 );
 $EXPECT  = 'end';
 
 1;
